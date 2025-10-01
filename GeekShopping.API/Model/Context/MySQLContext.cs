05c4c99cf470fd58b773f91cf779d76c3aa748f0
Diego Oliveira <diego.brivia@tecnobank.com.br>
2025-10-01 13:25:45 -0300

    Configuração inicial da API com EF Core e MySQL
    - Atualizado o framework para .NET 9.0 no projeto.
    - Configurado o Entity Framework Core com suporte ao MySQL.
    - Adicionado suporte ao Swagger para documentação da API.
    - Criada a entidade `Product` e mapeada para a tabela `product`.
    - Adicionada migração para criação da tabela `product` no banco.
    - Configurado o ambiente de desenvolvimento com URLs e Swagger.
    - Adicionado o contexto `MySQLContext` para gerenciar o banco.

---
+++ GeekShopping.API/Model/Context/MySQLContext.cs
@@ -0,0 +1,21 @@
+﻿using GeekShopping.API.Model.Base;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.API.Model.Context;
+
+public class MySQLContext : DbContext
+{
+    private readonly IConfiguration _configuration;
+
+    public MySQLContext() {}
+
+    public MySQLContext(
+        DbContextOptions<MySQLContext> options,
+        IConfiguration configuration) : base(options)
+    {
+        _configuration = configuration;
+    }
+
+    public DbSet<Product> Products { get; set; }
+    
+}
--- GeekShopping.API/Program.cs
+++ GeekShopping.API/Program.cs
@@ -1,3 +1,7 @@
+using GeekShopping.API.Model.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,19 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<MySQLContext>(options =>
+    options.UseMySql(connectionString, 
+    new MySqlServerVersion(
+        new Version(8, 0, 5))));
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo() { Title = "GeekShopAPI", Version = "1.0" });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -13,6 +30,9 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseAuthorization();
 
